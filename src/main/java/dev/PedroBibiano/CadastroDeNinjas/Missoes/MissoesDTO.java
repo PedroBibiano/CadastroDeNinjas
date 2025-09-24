3a542ca7c8de6b976c00e1f65dc9e6039e62723c
PedroBibiano <pedrobibiano777@gmail.com>
2025-09-24 17:33:31 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no Mapper os caminhos entidade x dto

--- src/main/java/dev/PedroBibiano/CadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/PedroBibiano/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,21 @@
 package dev.PedroBibiano.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.PedroBibiano.CadastroDeNinjas.Ninja.NinjaModel;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@NoArgsConstructor //Construtor vazio
+@AllArgsConstructor //Construtor cheio
+@Data //getters e setters
+//DTO-copia da entidade do DB
 public class MissoesDTO {
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+
 }
--- src/main/java/dev/PedroBibiano/CadastroDeNinjas/Ninja/NinjaDTO.java
+++ src/main/java/dev/PedroBibiano/CadastroDeNinjas/Ninja/NinjaDTO.java
@@ -1,4 +1,26 @@
 package dev.PedroBibiano.CadastroDeNinjas.Ninja;
 
+//DTO-copia da entidade do DB sem conexão com o DB;
+
+import dev.PedroBibiano.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Construtores + getters e setters
+@AllArgsConstructor //cosntrutor cheio
+@NoArgsConstructor //construtor vazio
+@Data //getters e setters
+
 public class NinjaDTO {
+
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private MissoesModel missoes;
+    private String Rank;
+
 }
