76e3619a746f2c3a028abe7ed0193ef6b0cc4c55
PedroBibiano <pedrobibiano777@gmail.com>
2025-10-10 19:08:14 -0300

    doc: Adiciona Swagger na minha aplicação para documentacao das minhas rotas no controller de ninjas

--- src/main/java/dev/PedroBibiano/CadastroDeNinjas/Ninja/NinjaController.java
+++ src/main/java/dev/PedroBibiano/CadastroDeNinjas/Ninja/NinjaController.java
@@ -1,5 +1,10 @@
 package dev.PedroBibiano.CadastroDeNinjas.Ninja;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.spel.spi.EvaluationContextExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +26,11 @@ public class NinjaController {
 
     // Adicionar ninja (CRETE)
     @PostMapping("/criar")
+    @Operation(summary = "Criar ninja",description = "Essa rota cria um ninja")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ninja criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criacao do ninja")
+    })
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
         NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -29,6 +39,7 @@ public class NinjaController {
 
     //LISTAR TODOS OS NINJAS (READ)
     @GetMapping("/listar")
+    @Operation(summary = "Lista todos os ninjas", description = "essa rota lista todos os dados dos ninjas cadastrados")
     public ResponseEntity<List<NinjaDTO>> listarNinjas() {
         List<NinjaDTO> ninjas = ninjaService.ListarNinjas();
         return ResponseEntity.ok(ninjas);
@@ -36,6 +47,11 @@ public class NinjaController {
 
     //LISTAR NINJA POR ID (READ)
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista o ninja por id",description = "Essa rota lista um ninja pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Ninja não encontrado")
+    })
     public ResponseEntity<?> listarNinjasPorID(@PathVariable Long id) {
         NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
         if (ninja != null) {
@@ -49,7 +65,16 @@ public class NinjaController {
 
     //Alterar dados dos ninjas (UPDATE)
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> AlterarNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
+    @Operation(summary = "Altera o ninja por id",description = "Essa rota altera um ninja por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Erro na alteração do ninja,ninja não encontrado")
+    })
+    public ResponseEntity<?> AlterarNinja(
+            @Parameter(description = "usuario manda o id no caminho da requisiçao")
+            @PathVariable Long id,
+            @Parameter(description ="usuario manda os dados do ninja a ser atualizado no corpo da requisiçao" )
+            @RequestBody NinjaDTO ninjaAtualizado) {
         NinjaDTO ninja = ninjaService.atualizarNinja(id, ninjaAtualizado);
         if (ninja != null) {
             return ResponseEntity.ok(ninja  + "\nninja atualizado com sucesso!");
@@ -58,6 +83,10 @@ public class NinjaController {
 
     //Deletar Ninja(DELETE)
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deleta ninja",description = "Essa rota deleta um ninja")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja deletado com sucesso"),
+    })
     public ResponseEntity<String> deletarNinjaPorId(@PathVariable Long id) {
         if (ninjaService.listarNinjasPorId(id) != null) {
             ninjaService.deletarNinjaPorId(id);
